74f48311a15e1e0daf7d0f07f2b72e12af70a863
frong <71235870+FrongDev@users.noreply.github.com>
2020-09-13 19:06:50 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!-- index.php -->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+		<meta charset="utf-8">
+		<title>Frongchamp! Any frongchampers??</title>
+		<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+	</head>
+	<body>
+		<header>
+			<?php include "include/header.php"; ?>
+		</header>
+		<table class="body">
+			<tbody>
+				<tr>
+					<td class="navbar">
+						<nav>
+							<?php include "include/nav.php"; ?>
+						</nav>
+					</td>
+					<td class="article">
+						<h1>poggers1</h1>
+					</td>
+				</tr>
+			</tbody>
+		</table>					
+		<!-- <iframe src="https://trinket.io/embed/python/5cc0aeed5d?outputOnly=true" width="100%" height="600" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe> -->
+	</body>
+</html>
