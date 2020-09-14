299276e26da0e1f2b31ad2b022d6d71a31d06d02
frong <71235870+FrongDev@users.noreply.github.com>
2020-09-13 20:31:42 -0700

    initial commit

--- include/header.php
+++
@@ -1,9 +0,0 @@
-<!-- header.php -->
-<table>
-	<tbody>
-		<tr>
-			<td class="logo"><a href="index.php"><img src="media/frong.png" width="130px" height="130px" alt="FRONGCHAMP"></a></td>
-			<td class="title"><h1>FRONG</h1></td>
-		</tr>
-	</tbody>
-</table>	
--- include/nav.php
+++
@@ -1,7 +0,0 @@
-<!-- nav.php -->
-<h1>Site Menu:</h1>
-<a href="releases.php">Games</a>
-<br>
-<a href="aboutus.php">About us</a>
-<br>
-<a href="news.php">News</a>
